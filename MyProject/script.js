8ccf243fea91f61713bdff32eca0548afe0933e1
SinkywithABinky <amarsbar3@gmail.com>
2018-02-16 15:21:35 -0600

    yeeter

--- MyProject/script.js
+++ MyProject/script.js
@@ -1,4 +1,5 @@
 let y = 20;
+let shapeKind = 1;
 
 
 function setup() {
@@ -10,20 +11,62 @@ function setup() {
 }
 
 function draw() {
-  rect(mouseX, mouseY, y, y);
+  if (keyIsPressed) {
+    changeSize();
+  }
+  if (shapeKind <1){
+    shapeKind = 3;
+
+  }
+  if (shapeKind >3){
+    shapeKind = 1;
+
+  }
+
 }
+function mousePressed() {
+  if (mouseButton === LEFT) {
+    if(shapeKind === 1){
+      rect(mouseX-y/2, mouseY-y/2, y, y);
+    }
+    else if(shapeKind === 2){
+      ellipse(mouseX, mouseY, y, y);
+    }
+    else if(shapeKind === 3){
+      triangle(mouseX+y/2,mouseY+y/2,mouseX-y/2,mouseY+y/2,mouseX,mouseY -y/2);
+    }
+
+  }
+}
+
 
 function changeSize() {
-
+  if (key === 'a' || key === 'A') {
+    y = y+1;
+  }
+  else if (key === 'd' || key === 'D') {
+    y = y-1;
+  }
 }
 
-function makeBackground(){
-  let circleWidth = 1;
+function keyPressed(){
+  if (key === 'w' || key === 'W') {
+    shapeKind = shapeKind+1;
+  }
+  else if (key === 's' || key === 'S') {
+    shapeKind = shapeKind-1;
+  }
+}
 
-  while( circleWidth <windowWidth){
-    ellipse(circleWidth,circleWidth,circleWidth,circleWidth);
-    ellipse(windowWidth/circleWidth,windowWidth/circleWidth,circleWidth,circleWidth);
-    circleWidth += 1;
+
+
+
+function makeBackground(){
+  let circleWidth = windowWidth*2;
+
+  while( circleWidth >0){
+    ellipse(windowWidth/2,windowHeight/2,circleWidth,circleWidth);
+    circleWidth -= 1;
 
   }
 
