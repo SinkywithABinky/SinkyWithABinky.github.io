7867f753dd69c145c857f487255db095b41cb108
SinkywithABinky <amarsbar3@gmail.com>
2018-03-02 15:19:57 -0600

    yeet

---
+++ chessBoard/script.js
@@ -0,0 +1,18 @@
+
+let color = 1;
+let boxSize = 40;
+let ydist
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(0);
+  stroke(255);
+}
+
+function draw() {
+  for(let i = 40;i>windowWidth; i += 40){
+    for(let j = 40;j>windowheight;)
+  }
+
+}
