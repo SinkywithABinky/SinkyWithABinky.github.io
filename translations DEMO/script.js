b660595bd06a7cdcb275eddafc3ed911f6c3da7c
SinkywithABinky <amarsbar3@gmail.com>
2018-03-14 14:20:20 -0600

    yeet

---
+++ translations DEMO/script.js
@@ -0,0 +1,16 @@
+//translationsDEMO march 13
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+  background(255);
+  ellipse(windowWidth/2,windowHeight/2,50);
+  push();
+  translate(windowWidth/2,windowHeight/2);
+  for (let i = 0; i < 60; i+5) {
+    line(0,50,0,40);
+    rotate(1/30*PI);
+  }
+}
